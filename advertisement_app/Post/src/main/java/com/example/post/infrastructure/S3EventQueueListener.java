cc28b9652656c46e01a4c993c8a2b179659b2378
maamoun <maamounyoussef393@gmail.com>
2025-12-10 13:31:19 +0200

    CREATE POST PART2 : NOT END  HANDLE THE COMMING EVENT FROM AWS S3

---
+++ advertisement_app/Post/src/main/java/com/example/post/infrastructure/S3EventQueueListener.java
@@ -0,0 +1,19 @@
+package com.example.post.infrastructure;
+
+import io.awspring.cloud.sqs.annotation.SqsListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class S3EventQueueListener {
+
+//@ NOT TESTED
+//@ NOT IMPLEMENTED
+    @SqsListener("s3-upload-events-queue")
+    public void onMessage(String eventJson) {
+//        Handle this one
+//        TODO ..
+//         CHANGE THE STATUS OF THIS POST IN THE DATABASE
+//         THEN
+//         PUBLISH A EVENT POSTCREATED
+    }
+}
