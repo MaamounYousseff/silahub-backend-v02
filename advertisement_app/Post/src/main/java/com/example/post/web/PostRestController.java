9b9371915d8f3e951c4dc1730e8c8357892d8fc0
maamoun <maamounyoussef393@gmail.com>
2025-12-16 16:41:50 +0200

    CLEAN UNCESSARY CODE

--- advertisement_app/Post/src/main/java/com/example/post/web/PostRestController.java
+++ advertisement_app/Post/src/main/java/com/example/post/web/PostRestController.java
@@ -6,8 +6,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.UUID;
 
 
 @RestController
@@ -24,13 +22,6 @@ public class PostRestController
     }
 
 
-    @GetMapping("/test")
-    public void asd()
-    {
-        UUID creatorId = UUID.fromString("f07cc7e5-842c-409e-a0a4-75615e14fe93");
-        this.postService.postCreated(creatorId);
-
-    }
 
 
 
